3a2e8ecb95f6a9cf3e1a1c901ccb9a834bbf7a68
vladimirburacenko <vladimir.burachenko@gmail.com>
2018-03-14 20:32:13 +0300

    Fixed names.

---
+++ src/main/java/by/epam/tc/dao/parser/ItemField.java
@@ -0,0 +1,40 @@
+package by.epam.tc.dao.parser;
+
+public enum ItemField {
+	
+	PLANES("items"),
+	ID("id"),
+	AMOUNT("amount"),
+	COLOR("color"),
+	RADAR("radar"),
+	PLANE("plane"),
+	ENGINES("engines"),
+	
+	PARAMETERS("parameters"),
+	MANUFACTURER("manufacturer"),
+	MODEL("model"),
+	ORIGIN("origin"),
+	TYPE("type"),
+	CREW("crew"),
+	PASSENGER_CAPACITY("passenger-capacity"),
+	CARGO_CAPACITY("cargo-capacity"),
+	PRICE("price"),
+	ENG_MODEL("model"),
+	ENG_TYPE("type"),
+	HEIGHT("height"),
+	LENGTH("length"),
+	WINGSPAN("wingspan"),
+	GROSS_WEIGHT("gross-weight");
+	
+	
+	private String value;
+	
+	ItemField(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+}
